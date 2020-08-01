109d72e083a84ee8a343fde5220e5ab6bc7c8d50
James Elias <james.elias95@gmail.com>
2020-08-01 14:05:41 -0700

    Project Init
    Initialize Visual Studio Solution

---
+++ File_Parser/Parser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace File_Parser
+{
+    class Parser
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
