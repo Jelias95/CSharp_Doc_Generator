05c2abfa234da3b8cd07e1956c153f4a3c87a72c
James Elias <james.elias95@gmail.com>
2020-08-01 15:49:18 -0700

    Read File Function
    * Implemented function to read and write all the lines in a file to a list

--- File_Parser/Parser.cs
+++ File_Parser/Parser.cs
@@ -9,20 +9,39 @@ namespace File_Parser
 {
     class Parser
     {
-        // Method to find all files in the directory - recursively checks all subdirectories as well
+        // Method to find all C# files in the directory - recursively checks all subdirectories as well
         public List<string> FindFilesInDirectory(string directory)
         {
             List<string> files = new List<string>();
+
+            // Find each file in a directory that ends with .cs
             foreach (string file in Directory.GetFiles(directory, "*.cs"))
             {
                 files.Add(file);
             }
 
+            // Recursively check each subdirectory and add the returned result to the existing list of files
             foreach (string dir in Directory.GetDirectories(directory))
             {
                 files = files.Concat<string>(FindFilesInDirectory(dir)).ToList();
             }
             return files;
+        }
+
+        // Method to find all lines in a given file
+        public List<string> ReadFile(string file)
+        {
+            List<string> lines = new List<string>();
+            string line;
+
+            // Read the file, write it to the list line by line, and close the file
+            StreamReader fileStream = new StreamReader(file);
+            while ((line = fileStream.ReadLine()) != null)
+            {
+                lines.Add(line);
+            }
+            fileStream.Close();
+            return lines;
         }
         static void Main(string[] args)
         {
@@ -30,7 +49,10 @@ namespace File_Parser
             List<string> files = parser.FindFilesInDirectory("C:/Users/james/Dropbox/Masters Work/Coursework/CSE 681 - Software Modeling and Analysis/C# Code Samples/LinqExamples");
             foreach (string file in files)
             {
-                Console.WriteLine(file);
+                foreach (string line in parser.ReadFile(file))
+                {
+                    Console.WriteLine(line);
+                }
             }
         }
     }
