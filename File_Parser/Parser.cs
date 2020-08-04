3be9a7c659f5e0f20e7673db14f5040b35f24d4e
James Elias <james.elias95@gmail.com>
2020-08-03 21:42:28 -0700

    Finalized Parser File
    * Removed unused using statements
    * Added structs for fileContents and lineDetails
    * Updated FindFilesInDirectory to exclude visual studio generated files
    * Added function to classify lines based on C# syntax
    * Added function to create FileContents struct

--- File_Parser/Parser.cs
+++ File_Parser/Parser.cs
@@ -1,40 +1,55 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace File_Parser
 {
-    class Parser
+    public class Parser
     {
-        // Method to find all C# files in the directory - recursively checks all subdirectories as well
+        // Structure to hold all the details of a line
+        public struct LineDetails
+        {
+            public string lineType;
+            public int lineNumber;
+            public string line;
+        }
+
+        // Structure to hold all the details of a file
+        public struct FileContents
+        {
+            public string directory;
+            public List<LineDetails> lines;
+        }
+
+        // Method to check if the directory is an excluded directory type
+        private static bool IsExcludedDirectory(string directory)
+        {
+            List<string> excludedDirectories = new List<string>() { "bin", "obj", "Properties" };
+            return excludedDirectories.Any(dir => new DirectoryInfo(directory).Name.Equals(dir));
+        }
+
+        // Method to find all C# files in the directory - recursively checks all subdirectories excluding bin, obj, and properties
         public List<string> FindFilesInDirectory(string directory)
         {
             List<string> files = new List<string>();
 
-            // Find each file in a directory that ends with .cs
             foreach (string file in Directory.GetFiles(directory, "*.cs"))
             {
                 files.Add(file);
             }
 
-            // Recursively check each subdirectory and add the returned result to the existing list of files
-            foreach (string dir in Directory.GetDirectories(directory))
+            foreach (string dir in Directory.GetDirectories(directory).Where(dir => !IsExcludedDirectory(dir)))
             {
                 files = files.Concat<string>(FindFilesInDirectory(dir)).ToList();
             }
             return files;
         }
 
-        // Method to find all lines in a given file
+        // Method to create a list of all lines in a given file
         public List<string> ReadFile(string file)
         {
-            List<string> lines = new List<string>();
             string line;
-
-            // Read the file, write it to the list line by line, and close the file
+            List<string> lines = new List<string>();
             StreamReader fileStream = new StreamReader(file);
             while ((line = fileStream.ReadLine()) != null)
             {
@@ -43,16 +58,75 @@ namespace File_Parser
             fileStream.Close();
             return lines;
         }
+
+        // Method to classify each line and return the FileContents struct
+        public string ClassifyLine(string line)
+        {
+            List<string> comments = new List<string>() { "//", "/*", "*" };
+            List<string> classes = new List<string>() { "class", "interface", "struct" };
+            List<string> controls = new List<string>() { "if", "for", "foreach", "while", "catch", "using" };
+
+            if (comments.Any(semi => line.StartsWith(semi)))
+            {
+                return "comment";
+            }
+
+            if (line.StartsWith("namespace"))
+            {
+                return "namespace";
+            }
+
+            if (classes.Any(semi => line.Contains(semi)))
+            {
+                return "class";
+            }
+
+            if (controls.Any(semi => line.Contains(semi)))
+            {
+                return "control";
+            }
+
+            if (line.StartsWith("{"))
+            {
+                return "startScope";
+            }
+
+            if (line.StartsWith("}"))
+            {
+                return "endScope";
+            }
+
+            return "line";
+        }
+
+        // Method to create package containing file contents (FileContents struct)
+        public FileContents CreateFileContents (string file)
+        {
+            FileContents fileContents = new FileContents
+            {
+                directory = file,
+                lines = new List<LineDetails>()
+            };
+            List<string> fileLines = ReadFile(file);
+            foreach (var line in fileLines.Select((value, index) => new { index, value }))
+            {
+                fileContents.lines.Add(new LineDetails
+                {
+                    line = line.value,
+                    lineNumber = line.index + 1,
+                    lineType = ClassifyLine(line.value.Trim())
+                });
+            }
+            return fileContents;
+        }
+
         static void Main(string[] args)
         {
             Parser parser = new Parser();
-            List<string> files = parser.FindFilesInDirectory("C:/Users/james/Dropbox/Masters Work/Coursework/CSE 681 - Software Modeling and Analysis/C# Code Samples/LinqExamples");
+            List<string> files = parser.FindFilesInDirectory("../../");
             foreach (string file in files)
             {
-                foreach (string line in parser.ReadFile(file))
-                {
-                    Console.WriteLine(line);
-                }
+                FileContents fileContents = parser.CreateFileContents(file);
             }
         }
     }
